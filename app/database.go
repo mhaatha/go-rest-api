7629c4c4c7dc39ef1f53ccb003e1ec565593ee0d
Hafidz Athaya <mhafidza528@gmail.com>
2025-03-01 17:05:09 +0800

    Create DB connection

---
+++ app/database.go
@@ -0,0 +1,21 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/mhaatha/go-rest-api/helper"
+)
+
+func NewDB(dbPassword string) *sql.DB {
+	db, err := sql.Open("mysql", fmt.Sprintf("root:%v@tcp(localhost:3360)/go_rest_api", dbPassword))
+	helper.PanicIfError(err)
+
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(20)
+	db.SetConnMaxLifetime(60 * time.Minute)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db
+}
