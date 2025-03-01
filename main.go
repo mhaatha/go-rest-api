e38442d8f0ae753ddc78e9acf7dadccc211e5da8
Hafidz Athaya <mhafidza528@gmail.com>
2025-03-01 17:05:55 +0800

    Setup main file

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/julienschmidt/httprouter"
+	"github.com/mhaatha/go-rest-api/app"
+	"github.com/mhaatha/go-rest-api/controller"
+	"github.com/mhaatha/go-rest-api/repository"
+	"github.com/mhaatha/go-rest-api/service"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	db := app.NewDB(dbPassword)
+	validate := validator.New()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+}
